109ef329a500c3da0be6a4b26ff6b6653c77cfbd
Phil Swartz <pswartz.sju@gmail.com>
2020-04-06 20:41:00 -0400

    added setup files for component library and tested that styleguidist server runs

---
+++ styleguide.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+    webpackConfig: {
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader"
+                }
+            ]
+        }
+    }
+}
